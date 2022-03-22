68875e1ea0572168701e921b4ec0c75486cfca93
luisbaldeon <luisbaldeon2313@gmail.com>
2022-03-22 12:18:40 -0500

    Se integro la clas Mensaje

--- src/com/acme/HolaMundo.java
+++ src/com/acme/HolaMundo.java
@@ -3,5 +3,8 @@ package com.acme;
 public class HolaMundo {
     public static void main(String[] args){
         System.out.println("Hola Mundo");
+        Mensaje msj = new Mensaje();
+        String nombre = msj.solicitaDatos("Escribe tu nombre");
+        msj.saludar(nombre);
     }
 }
